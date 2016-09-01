026764994cf50ff8823e2a7567d451ae04db0756
john castrillon <Jcastrillon87@yahoo.com>
2016-09-01 10:51:28 -0400

    add pseudo code to player.js and opponent.js, render function working a little better. Converted standalone functions to object methods.

--- opponent.js
+++ opponent.js
@@ -1,1 +1,17 @@
+/*Pseudocode section:
+What does the opponent need to do?
+-the opponent should have hitpoints and random damage points for punches
+-the opponent should be able to punch at set intervals
+-opponent needs a render function
+-opponent needs to be whichever Nicolas Cage the player didn't choose
+-opponent needs to be on opposite side of arena as the player
+-I need to know the opponent's position when a punch is delivered.
+*/ 
 
+
+class Opponent {
+  constructor() {
+    this.hitpoints = 1000;
+  }
+
+}
--- player.js
+++ player.js
@@ -1,6 +1,33 @@
+/*pseudo code section:
+What does the player need to do?
+-Player should have a name.
+-Player needs a block function.
+-Player needs a punch method (round house kick and special move will be bonus moves for later if I have time)
+-Player needs a health meter (an actual bar will a bonus later)
+-Player needs a render function
+-I'll need to know the player's position when a punch is thrown and then render the animation
+then I will need to have them move back into position once the punch is thrown.
+-The ability to choose between ConAir Nicolas Cage and The Wicker Man Nicolas Cage
+-Set hit points(health) and random damage points for punches
+*Bonuses:
+-A moving animation
+-A kick and special move animation/methods
+-A health bar and a special move bar
+-More Cages: Ghost Rider, Castor Troy and Lord of War editions are top candidates if time allows
+-Sound clips for the two Cages:
+Conair: "This ain't happening. Not here, not now."
+        "I said... Put. The bunny. Back. In the box."
+        "Hoo-ray for the sounds of fucking silence!"
+
+The Wicker Man:
+        "How'd it get burned? How'd it get burned? HOW'D IT GET BURNED? HOW'D IT GET BURNED?!"
+        "KILLING ME WON'T BRING BACK YOUR GODDAMN HONEY!"
+        "No! Not the bees! NOT THE BEES! AHHHHH!"*/
+
 class Player {
-  constructor({name}) {
+  constructor({name, hitpoints}) {
     this.name = name;
+    this.hitpoints = 1000;
   }
   makePerson() {
     let player = document.createElement('div');
@@ -10,25 +37,42 @@ class Player {
   render() {
     return this.makePerson();
   }
+  getCurrentPosition(person) {
+    let elem = document.querySelectorAll(person)[0];
+    let howRight = elem.getAttribute('style').split('left: ')[1]; // left: 0px
+    let extractedPixels = Number(howRight.replace('px', ''));
+    return {
+      elem: elem,
+      position: extractedPixels
+    };
+  }
+  move(elemObject) {
+    let elem = elemObject.elem;
+    let position = elemObject.position;
+    console.log(elem, position);
+    elem.setAttribute('style', `left: ${position + 15}px`);
+    return elem;
+  }
 }
-function getCurrentPosition(person) {
-  let elem = document.querySelectorAll(person)[0];
-  let howRight = elem.getAttribute('style').split('left: ')[1]; // left: 0px
-  let extractedPixels = Number(howRight.replace('px', ''));
-  return {
-    elem: elem,
-    position: extractedPixels
-  };
-}
+//subfunctions to be added as object methods later
+// function getCurrentPosition(person) {
+//   let elem = document.querySelectorAll(person)[0];
+//   let howRight = elem.getAttribute('style').split('left: ')[1]; // left: 0px
+//   let extractedPixels = Number(howRight.replace('px', ''));
+//   return {
+//     elem: elem,
+//     position: extractedPixels
+//   };
+// }
 
-function move(elemObject) {
-  let elem = elemObject.elem;
-  let position = elemObject.position;
-  console.log(elem, position);
-  elem.setAttribute('style', `left: ${position + 15}px`);
-  return elem;
-}
+// function move(elemObject) {
+//   let elem = elemObject.elem;
+//   let position = elemObject.position;
+//   console.log(elem, position);
+//   elem.setAttribute('style', `left: ${position + 15}px`);
+//   return elem;
+// }
 
-let john = new Player({name: 'John'});
-john.render();
-move(getCurrentPosition(john));
+// const john = new Player({name: 'John', hitpoints: 1000});
+// john.render();
+//  move(getCurrentPosition(john));
